638da748af3b875b51fdf544d30b975d4b916387
shanwu <shanwustore@gmail.com>
2019-10-07 15:09:54 +0800

    Updates

---
+++ precache-manifest.36c92aad6a1a0a647a2e8974d14a1e7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "909ce84e766afbcad4a1e9b021ff2ccc",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "c2b256b60687f48970a4",
+    "url": "/web/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b227ff3f727bcaa35459",
+    "url": "/web/static/js/2.cade9933.chunk.js"
+  },
+  {
+    "revision": "c2b256b60687f48970a4",
+    "url": "/web/static/js/main.0ed9a1a3.chunk.js"
+  },
+  {
+    "revision": "9c25ffdd5da5b07ff37e",
+    "url": "/web/static/js/runtime-main.1f589436.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/web/static/media/logo.25bf045c.svg"
+  }
+]);
