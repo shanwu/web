c0c6658b4e6703fe8be354653637299e09eea719
shanwu <shanwustore@gmail.com>
2019-11-03 19:02:52 +0800

    change: Add Gold-Investment entry

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import RoundIconButton from '../src/component/RoundIconButton.js';
 import houseLogo from '../src/image/icon_house.svg';
+import goldLogo from '../src/image/icon_gold.svg';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -12,17 +13,30 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <div style={{
+        background:'white',
+        padding:'15px'
+      }}>
         <RoundIconButton
           icon={houseLogo}
-          marginTop='20px'
-          background='#CCFFFF'
+          background='white'
           text='房产投资'
           height='48px'
           width='150px'
           roundRadius='10px'
           textColor='#3a3a3a'
           onClickListener={() => { this.nextPath('house') }} />
+
+        <RoundIconButton
+          icon={goldLogo}
+          marginLeft='20px'
+          background='white'
+          text='黄金买卖'
+          height='48px'
+          width='150px'
+          roundRadius='10px'
+          textColor='#3a3a3a'
+          onClickListener={() => { this.nextPath('gold') }} />
       </div>
 
     );
